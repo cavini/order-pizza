23c35d088e175bceedef40967d971ab3b0c07c7f
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2023-06-14 13:49:07 -0300

    feature: successfully added typescript, eslint and prettier configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(200);
 
   return (
     <>
