a313fe9c6db9d4acc2cb35ea63128e5e5fc33e68
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2023-06-20 20:05:35 -0300

    feature: added more styles with tailwindcss

--- src/components/CreateOrder/index.tsx
+++ src/components/CreateOrder/index.tsx
@@ -43,12 +43,15 @@ const CreateOrder = () => {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority">Want to give your order priority?</label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            className="inline-block rounded-full bg-yellow-400  px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+            disabled={isSubmitting}
+          >
             {' '}
             {isSubmitting ? 'Placing order...' : 'Order now'}
           </button>
