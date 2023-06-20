a519c0236c9694ce8fa934d754d98be51aa478e4
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2023-06-20 12:50:37 -0300

    feature: added loading component, basic styling and isLoading state

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from '../Header';
 import CartOverview from '../CartOverview';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
+import { NavigationStatus } from '../../interfaces/useNavigation';
+import Loading from '../Loading';
 
 const Layout = () => {
+  const navigation = useNavigation();
+
+  const isLoading = navigation.state === NavigationStatus.LOADING;
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loading />}
       <Header />
       <main>
         <h1>Content</h1>
