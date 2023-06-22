80200ac9b02d8f306d990dbaae706a3580d86b13
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2023-06-22 18:42:01 -0300

    feature: added favicon, title, new styles, WIP with interface, created order item page

--- src/components/Buttons/LinkButton/index.tsx
+++ src/components/Buttons/LinkButton/index.tsx
@@ -8,7 +8,7 @@ const LinkButton = ({ children, to, type }: LinkButtonProps) => {
     <Link
       className={`${
         type === ButtonType.PRIMARY
-          ? 'button px-4 py-3 md:px-6 md:py-4'
+          ? 'button bg-yellow-400 px-4 py-3 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring-yellow-300 md:px-6 md:py-4'
           : type === ButtonType.SMALL
           ? 'button px-4 py-2 text-xs md:px-5 md:py-2.5'
           : type === ButtonType.LINK
