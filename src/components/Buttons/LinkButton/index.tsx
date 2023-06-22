3d34842a81c4ede93d6a254c4d8394c19a4a13dd
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2023-06-22 17:51:30 -0300

    chore: added interface

--- src/components/Buttons/LinkButton/index.tsx
+++ src/components/Buttons/LinkButton/index.tsx
@@ -6,13 +6,13 @@ import { ButtonType } from '../Button/@types';
 const LinkButton = ({ children, to, type }: LinkButtonProps) => {
   return (
     <Link
-      className={`button ${
+      className={`${
         type === ButtonType.PRIMARY
-          ? ' bg-yellow-400  px-4 py-3 hover:bg-yellow-300 focus:bg-yellow-300 md:px-6 md:py-4'
+          ? 'button px-4 py-3 md:px-6 md:py-4'
           : type === ButtonType.SMALL
-          ? '  bg-yellow-400  px-4 py-2 text-xs hover:bg-yellow-300 focus:bg-yellow-300 md:px-5 md:py-2.5'
-          : type === ButtonType.SECONDARY
-          ? '  border-2 border-stone-300 px-4 py-3 md:px-6 md:py-4'
+          ? 'button px-4 py-2 text-xs md:px-5 md:py-2.5'
+          : type === ButtonType.LINK
+          ? 'link-button'
           : ''
       }`}
       to={to}
